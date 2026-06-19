a95ff4a804cf21475e46ee3a93cab8ef3249b908
uchenna <valentinevalobt@gmail.com>
2026-06-19 16:34:32 +0000

    Fix: revert to StaticFilesStorage to resolve manifest error

--- tech_school/settings.py
+++ tech_school/settings.py
@@ -106,7 +106,7 @@ STORAGES = {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.StaticFilesStorage",
     },
 }
 
