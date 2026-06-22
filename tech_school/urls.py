3f284e11dc882e09384849eaae9d6fade1fcad5f
uchenna <valentinevalobt@gmail.com>
2026-06-22 13:51:33 +0000

    Temporarily disable OTP in urls_2.py

--- tech_school/urls.py
+++ tech_school/urls.py
@@ -2,10 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django_otp.admin import OTPAdminSite
+# from django_otp.admin import OTPAdminSite
 
 # Force the standard admin site to use OTP
-admin.site.__class__ = OTPAdminSite
+# admin.site.__class__ = OTPAdminSite
 
 urlpatterns = [
     # 1. The official Django backend management (requires OTP)
