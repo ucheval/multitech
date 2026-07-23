7e768dde79737462ecea7dad564aad1f240fcf8a
uchenna <valentinevalobt@gmail.com>
2026-07-23 17:15:59 +0000

    i removed forced password lenth from settings auth again

--- tech_school/settings.py
+++ tech_school/settings.py
@@ -151,11 +151,7 @@ DATABASES = {
 # PASSWORD VALIDATION
 # =========================
 
-AUTH_PASSWORD_VALIDATORS = [
-    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
-    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
-    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
-]
+AUTH_PASSWORD_VALIDATORS = []
 
 
 # =========================
